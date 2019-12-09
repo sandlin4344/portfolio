4c2bfa06af000f2d955956b6eb907d5dcb37269a
sandlin4344 <styles17sandlin@gmail.com>
2019-12-09 02:06:46 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Styles's Engineering Portfolio</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Styles Sandlin 2nd Period]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="./Sandlin_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="./Sandlin_Projects.html">Projects.html</a></li></br>
+      <li><a href="./Sandlin_Resume.html">Resume.html</a></li></br>
+      <li><a href="./Sandlin_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
